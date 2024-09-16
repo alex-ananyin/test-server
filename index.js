1f8696e046c3b90d72cae9e885db5c18b5115810
Alex Ananyin <iksananin@gmail.com>
2024-09-16 13:20:54 +0200

    first version

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Указываем путь к папке с HTML-файлами
+app.use(express.static(path.join(__dirname)));
+
+// Обработка GET-запроса на корневой URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Запуск сервера на порту 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
